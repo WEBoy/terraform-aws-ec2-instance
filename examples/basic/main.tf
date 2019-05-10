74d640324b7a5c3a4b875e561b4b22aee92bb984
sergey <sergey@it-manager.localdomain>
2019-05-10 11:01:12 +0300

    Only one VPS with opened ssh port

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,5 +1,6 @@
 provider "aws" {
-  region = "eu-west-1"
+  profile = "test"
+  region = "eu-central-1"
 }
 
 ##################################################################
@@ -39,12 +40,12 @@ module "security_group" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "2.7.0"
 
-  name        = "example"
-  description = "Security group for example usage with EC2 instance"
+  name        = "SKalinin Terraform"
+  description = "SKallinin security group for EC2 instance"
   vpc_id      = "${data.aws_vpc.default.id}"
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp", "all-icmp"]
+  ingress_rules       = ["http-80-tcp", "all-icmp", "ssh-tcp"]
   egress_rules        = ["all-all"]
 }
 
@@ -56,11 +57,11 @@ resource "aws_eip" "this" {
 module "ec2" {
   source = "../../"
 
-  instance_count = 2
+  # instance_count = 2
 
-  name                        = "example-normal"
+  name                        = "skalinin-terraform-normal"
   ami                         = "${data.aws_ami.amazon_linux.id}"
-  instance_type               = "m4.large"
+  instance_type               = "m4.micro"
   subnet_id                   = "${element(data.aws_subnet_ids.all.ids, 0)}"
   vpc_security_group_ids      = ["${module.security_group.this_security_group_id}"]
   associate_public_ip_address = true
@@ -71,30 +72,30 @@ module "ec2" {
   }]
 }
 
-module "ec2_with_t2_unlimited" {
-  source = "../../"
-
-  instance_count = 1
-
-  name                        = "example-t2-unlimited"
-  ami                         = "${data.aws_ami.amazon_linux.id}"
-  instance_type               = "t2.micro"
-  cpu_credits                 = "unlimited"
-  subnet_id                   = "${element(data.aws_subnet_ids.all.ids, 0)}"
-  vpc_security_group_ids      = ["${module.security_group.this_security_group_id}"]
-  associate_public_ip_address = true
-}
-
-module "ec2_with_t3_unlimited" {
-  source = "../../"
-
-  instance_count = 1
-
-  name                        = "example-t3-unlimited"
-  ami                         = "${data.aws_ami.amazon_linux.id}"
-  instance_type               = "t3.large"
-  cpu_credits                 = "unlimited"
-  subnet_id                   = "${element(data.aws_subnet_ids.all.ids, 0)}"
-  vpc_security_group_ids      = ["${module.security_group.this_security_group_id}"]
-  associate_public_ip_address = true
-}
+# module "ec2_with_t2_unlimited" {
+#   source = "../../"
+#
+#   instance_count = 1
+#
+#   name                        = "skalinin-example-t2-unlimited"
+#   ami                         = "${data.aws_ami.amazon_linux.id}"
+#   instance_type               = "t2.micro"
+#   cpu_credits                 = "unlimited"
+#   subnet_id                   = "${element(data.aws_subnet_ids.all.ids, 0)}"
+#   vpc_security_group_ids      = ["${module.security_group.this_security_group_id}"]
+#   associate_public_ip_address = true
+# }
+#
+# module "ec2_with_t3_unlimited" {
+#   source = "../../"
+#
+#   instance_count = 1
+#
+#   name                        = "skalinin-example-t3-unlimited"
+#   ami                         = "${data.aws_ami.amazon_linux.id}"
+#   instance_type               = "t3.large"
+#   cpu_credits                 = "unlimited"
+#   subnet_id                   = "${element(data.aws_subnet_ids.all.ids, 0)}"
+#   vpc_security_group_ids      = ["${module.security_group.this_security_group_id}"]
+#   associate_public_ip_address = true
+# }
--- examples/basic/outputs.tf
+++ examples/basic/outputs.tf
@@ -3,10 +3,10 @@ output "ids" {
   value       = "${module.ec2.id}"
 }
 
-output "ids_t2" {
-  description = "List of IDs of t2-type instances"
-  value       = "${module.ec2_with_t2_unlimited.id}"
-}
+# output "ids_t2" {
+#   description = "List of IDs of t2-type instances"
+#   value       = "${module.ec2_with_t2_unlimited.id}"
+# }
 
 output "public_dns" {
   description = "List of public DNS names assigned to the instances"
@@ -38,7 +38,7 @@ output "credit_specification" {
   value       = "${module.ec2.credit_specification}"
 }
 
-output "credit_specification_t2_unlimited" {
-  description = "Credit specification of t2-type EC2 instance"
-  value       = "${module.ec2_with_t2_unlimited.credit_specification}"
-}
+# output "credit_specification_t2_unlimited" {
+#   description = "Credit specification of t2-type EC2 instance"
+#   value       = "${module.ec2_with_t2_unlimited.credit_specification}"
+# }
